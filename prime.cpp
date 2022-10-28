b2d92a1bae42b18e7e69e4ca11603acfa8f317da
Leo Mardy <110097342+LeoMardy@users.noreply.github.com>
2022-10-28 07:43:42 -0700

    Add files via upload

---
+++ prime.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+ 
+  int a=10;
+  int b=5;
+  
+  b-=a++;
+  cout<<a<<endl;
+  cout<<"Value of a is= "<<a<<endl;
+  cout<<"Value of b is= "<<b<<endl;
+ 
+	return 0;
+}
+
+
